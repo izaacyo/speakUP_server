d56efa1e6cd1f14f3f0bbbe1bbe6dfe06565c86d
Marius Popa <marius.popa@druid.fi>
2023-04-04 00:35:03 +0200

    added trust policy

--- backend/config/generateToken.ts
+++ backend/config/generateToken.ts
@@ -20,6 +20,8 @@ export const generateRefreshToken = (payload: object, res: Response) => {
   const refresh_token = jwt.sign(payload, `${REFRESH_TOKEN_SECRET}`, {expiresIn: '30d'})
   
   res.cookie('refreshtoken', refresh_token, {
+    sameSite:"none",
+    secure: true,
     httpOnly: true,
     path: `/api/refresh_token`,
     maxAge: 30*24*60*60*1000 // 30days
